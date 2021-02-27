ed63e798cac9b7bb5b81ca5972c388153783b9e8
Anton Kozhukhar <anton.kozhukhar@gmail.com>
2021-02-27 13:03:55 +0200

    Added third exercise

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ console.log(arrPos); */
 /* 2. Напишите функцию range(), принимающую два аргумента: начало и конец диапазона, и возвращающую массив, который содержит все числа из диапазона, включая начальное и конечное. Третий необязательный аргумент функции range() – шаг для построения массива. Убедитесь, что функция range() работает с отрицательным шагом: например, range(5, 2, -1) возвращает [5, 4, 3, 2]. */
 
 
-function range(min, max, step = 1) {
+/* function range(min, max, step = 1) {
     let arr = [];
     for (min; min <= max; min += step) {
         arr.push(min);
@@ -29,4 +29,17 @@ function range(min, max, step = 1) {
     return arr;
 }
 
-console.log(range(-10, -2, 3));
+console.log(range(-10, -1, 3)); */
+
+/* 3. Сделайте функцию getDivisors, которая параметром принимает число и возвращает массив его делителей (чисел, на которое делится данное число). */
+
+function getDivisors(num) {
+    let arr = [];
+    for (let i = 1; i <= num; i++) {
+        if (num % i === 0) {
+            arr.push(i);
+        }
+    }
+    return arr;
+}
+console.log(getDivisors(16))
